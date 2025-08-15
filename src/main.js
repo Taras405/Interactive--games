a8e236f44ece959eeb4133328eac78f99721487d
taki <tarassevcuk700@gmail.com>
2025-08-16 00:10:15 +0300

    update structure vite

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './scss/main.scss';
+import './sections/footer/footer.js';
+import './sections/header/header.js';
+import './sections/section1/section1.js';
+import './sections/section2/section2.js';
+import './sections/section3/section3.js';
+import './sections/section4/section4.js';
+import './sections/section5/section5.js';
+import './sections/section6/section6.js';
