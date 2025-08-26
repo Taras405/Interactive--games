e4bf56780defc28f0f32e9a460d4d8cb040bcfd6
taki <tarassevcuk700@gmail.com>
2025-08-26 15:37:41 +0300

    check fix2

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,41 @@
+// import { defineConfig } from 'vite';
+// import glob from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig({
+//   define: {
+//     global: {},
+//   },
+//   root: 'src',
+//   build: {
+//     rollupOptions: {
+//       input: glob.sync('./src/index.html'),
+//     },
+//     outDir: '../dist',
+//   },
+//   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+//   base: '/Interactive--games/',
+// });
+
 import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  define: {
-    global: {},
-  },
   root: 'src',
-  build: {
-    rollupOptions: {
-      input: glob.sync('./src/index.html'),
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
   base: '/Interactive--games/',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        index: 'src/index.html'
+      }
+    }
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html'])
+  ]
 });
