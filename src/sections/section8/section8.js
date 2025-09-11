9c86d26929d91c6c455b3f6b4a49dba4f59c0001
Ваше Ім'я <bondnazar2404@gmail.com>
2025-09-11 23:57:58 +0300

    style_s8

---
+++ src/sections/section8/section8.js
@@ -0,0 +1,138 @@
+const scientistsHtml = document.querySelector('.section8');
+scientistsHtml.innerHTML = `
+    <div class="container">
+    <h2 class="titleGame">Обери вченого/их</h2>
+    <form class="scientists">
+      <div class="scientists__slots">
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+        <div class="slot"></div>
+      </div>
+      <div class="scientists__tasks">
+        <button class="task-btn" type="button">
+          Які вчені народилися в 19 ст.
+        </button>
+        <button class="task-btn" type="button">
+          Знайти рік народження Albert Einshtein
+        </button>
+        <button class="task-btn" type="button">
+          Відсортувати вчених за алфавітом
+        </button>
+        <button class="task-btn" type="button">
+          Знайти вчених, прізвища яких починаються на на літеру “С”
+        </button>
+        <button class="task-btn" type="button">
+          Відсортувати вчених за кількістю прожитих років
+        </button>
+        <button class="task-btn" type="button">
+          Видалити всіх вчених, ім’я яких починається на “А”
+        </button>
+        <button class="task-btn" type="button">
+          Знайти вченого, який народився найпізніше
+        </button>
+        <button class="task-btn" type="button">
+          Знайти вченого, який прожив найдовше і вченого, який прожив найменше
+        </button>
+        <button class="task-btn" type="button">
+          Знайти вчених, в яких співпадають перші літери імені і прізвища
+        </button>
+      </div>
+    </form>
+  </div>
+`;
+
+const scientists = [
+  {
+    name: 'Albert',
+    surname: 'Einstein',
+    born: 1879,
+    dead: 1955,
+    id: 1,
+  },
+  {
+    name: 'Isaac',
+    surname: 'Newton',
+    born: 1643,
+    dead: 1727,
+    id: 2,
+  },
+  {
+    name: 'Galileo',
+    surname: 'Galilei',
+    born: 1564,
+    dead: 1642,
+    id: 3,
+  },
+  {
+    name: 'Marie',
+    surname: 'Curie',
+    born: 1867,
+    dead: 1934,
+    id: 4,
+  },
+  {
+    name: 'Johannes',
+    surname: 'Kepler',
+    born: 1571,
+    dead: 1630,
+    id: 5,
+  },
+  {
+    name: 'Nicolaus',
+    surname: 'Copernicus',
+    born: 1473,
+    dead: 1543,
+    id: 6,
+  },
+  {
+    name: 'Max',
+    surname: 'Planck',
+    born: 1858,
+    dead: 1947,
+    id: 7,
+  },
+  {
+    name: 'Katherine',
+    surname: 'Blodgett',
+    born: 1898,
+    dead: 1979,
+    id: 8,
+  },
+  {
+    name: 'Ada',
+    surname: 'Lovelace',
+    born: 1815,
+    dead: 1852,
+    id: 9,
+  },
+  {
+    name: 'Sarah E.',
+    surname: 'Goode',
+    born: 1855,
+    dead: 1905,
+    id: 10,
+  },
+  {
+    name: 'Lise',
+    surname: 'Meitner',
+    born: 1878,
+    dead: 1968,
+    id: 11,
+  },
+  {
+    name: 'Hanna',
+    surname: 'Hammarström',
+    born: 1829,
+    dead: 1909,
+    id: 12,
+  },
+];
